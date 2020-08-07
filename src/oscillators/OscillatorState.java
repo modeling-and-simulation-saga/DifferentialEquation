65f41ea95c1a99d6f605fd1090f478dde5e9a12b
tada195908 <48470775+tada195908@users.noreply.github.com>
2020-08-07 12:14:02 +0900

    Add files via upload

---
+++ src/oscillators/OscillatorState.java
@@ -0,0 +1,18 @@
+package oscillators;
+
+/**
+ *
+ * @author tadaki
+ */
+public class OscillatorState {
+    public final double t;
+    public final double x;
+    public final double v;
+
+    public OscillatorState(double t, double x, double v) {
+        this.t = t;
+        this.x = x;
+        this.v = v;
+    }
+    
+}
