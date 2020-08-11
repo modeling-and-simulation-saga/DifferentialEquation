9e2b872d3b2fd2dd85c4674717b9620fa5b1a00c
tada195908 <48470775+tada195908@users.noreply.github.com>
2020-08-11 11:51:07 +0900

    Add files via upload

--- src/coupledOscillators/CoupledOscillators.java
+++ src/coupledOscillators/CoupledOscillators.java
@@ -3,6 +3,7 @@ package coupledOscillators;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.List;
+import oscillators.OscillatorState;
 import myLib.rungeKutta.DifferentialEquation;
 import myLib.rungeKutta.RungeKutta;
 import myLib.utils.FileIO;
@@ -16,7 +17,7 @@ import myLib.utils.Utils;
 public class CoupledOscillators {
 
     private final int numOscillators;
-    private final double y[];
+    private final double y[];//各振動子の変位と速度
     private final DifferentialEquation equation;
     private double t;
     private double b;
@@ -28,15 +29,16 @@ public class CoupledOscillators {
      * @param k ばね定数/質量
      * @param b ばねの自然長
      */
-    public CoupledOscillators(Oscillator oscillators[], double k, double b) {
+    public CoupledOscillators(OscillatorState oscillators[],
+            double k, double b) {
         this.b = b;
         numOscillators = oscillators.length;//振動子の数
         y = new double[2 * numOscillators];//従属変数
-        t = 0.;//独立変数の初期値
+        t = oscillators[0].t;//独立変数の初期値
         //偶数番目の従属変数は変位、奇数番目は速度
         //初期値設定
         for (int i = 0; i < numOscillators; i++) {
-            y[2 * i] = oscillators[i].y;//変位
+            y[2 * i] = oscillators[i].x;//変位
             y[2 * i + 1] = oscillators[i].v;//速度
         }
         //微分方程式の記述
@@ -55,6 +57,7 @@ public class CoupledOscillators {
             int j = 2 * (numOscillators - 1);
             dy[j] = yy[j + 1];
             dy[j + 1] = -k * (-yy[j - 2] + 2 * yy[j]);
+
             return dy;
         };
     }
@@ -63,14 +66,14 @@ public class CoupledOscillators {
      * 時間発展：1ステップ
      *
      * @param dt 時間
-     * @return 更新後の独立変数の値
+     * @return 更新後の従属変数の値
      */
-    public Oscillator[] update(double dt) {
+    public OscillatorState[] update(double dt) {
         //更新後の従属変数の値
         double yy[] = RungeKutta.rk4(t, y, dt, equation);
         //値の更新
-        for (int i = 0; i < 2 * numOscillators; i++) {
-            y[i] = yy[i];
+        for(int i=0;i<2*numOscillators;i++){
+            y[i]=yy[i];
         }
         t += dt;
         //現在の状態をOscillatorとして返す
@@ -84,15 +87,16 @@ public class CoupledOscillators {
      * @param nstep ステップ数
      * @return
      */
-    public List<Oscillator[]> evolution(double period, int nstep) {
-        List<Oscillator[]> history = Utils.createList();
+    public List<OscillatorState[]> evolution(double period, int nstep) {
+        List<OscillatorState[]> history = Utils.createList();
         double dt = period / nstep;
-        for (int s = 0; s < nstep; s++) {
-            Oscillator[] o = update(dt);
-            history.add(o);
+        for (int i = 0; i < nstep; i++) {
+            OscillatorState[] osc = update(dt);
+            history.add(osc);
         }
         return history;
     }
+
     /**
      * 時間間隔とステップ数を指定して時間発展
      *
@@ -101,12 +105,12 @@ public class CoupledOscillators {
      * @param tic 値を返すステップ間隔
      * @return
      */
-    public List<Oscillator[]> evolution(double period, int nstep, int tic) {
-        List<Oscillator[]> history = Utils.createList();
+    public List<OscillatorState[]> evolution(double period, int nstep, int tic) {
+        List<OscillatorState[]> history = Utils.createList();
         double dt = period / nstep;
-        for (int s = 0; s < nstep; s++) {
-            Oscillator[] o = update(dt);
-            if (s % tic == 0) {
+        for (int i = 0; i < nstep; i++) {
+            OscillatorState[] o = update(dt);
+            if (i % tic == 0) {
                 history.add(o);
             }
         }
@@ -114,14 +118,16 @@ public class CoupledOscillators {
     }
 
     /**
-     * 現在の状態をOscillatorとして返す
+     * 現在の状態をOscillatorStateとして返す
      *
      * @return
      */
-    public Oscillator[] getOscillators() {
-        Oscillator oscillators[] = new Oscillator[numOscillators];
+    public OscillatorState[] getOscillators() {
+        OscillatorState oscillators[]
+                = new OscillatorState[numOscillators];
         for (int i = 0; i < numOscillators; i++) {
-            oscillators[i] = new Oscillator(y[2 * i], y[2 * i + 1], t);
+            oscillators[i]
+                    = new OscillatorState(t, y[2 * i], y[2 * i + 1]);
         }
         return oscillators;
     }
@@ -132,10 +138,11 @@ public class CoupledOscillators {
      */
     public static void main(String[] args) throws IOException {
         //3個の連成振動
-        Oscillator[] initialOscillators = {
-            new Oscillator(1., 0.),
-            new Oscillator(2., 0.),
-            new Oscillator(-1., 0.)
+        double t0 = 0.;
+        OscillatorState[] initialOscillators = {
+            new OscillatorState(t0, 1., 0.),
+            new OscillatorState(t0, 2., 0.),
+            new OscillatorState(t0, -1., 0.)
         };
         double b = 10.;
         double k = 1.;
@@ -145,21 +152,22 @@ public class CoupledOscillators {
         double period = 40.;
         int nstep = 20000;
         // 結果は200ステップ毎に取得
-        int tic =200;
-        List<Oscillator[]> history = sys.evolution(period, nstep, tic);
+        int tic = 200;
+        List<OscillatorState[]> history
+                = sys.evolution(period, nstep, tic);
         //出力ファイル名の指定
         String filename = CoupledOscillators.class.getSimpleName()
                 + "-output.txt";
         // 結果をファイルへ出力
         try (BufferedWriter out = FileIO.openWriter(filename)) {
-            for (Oscillator[] oscillators : history) {
+            for (OscillatorState[] oscillators : history) {
                 StringBuilder sb = new StringBuilder();
-                double t = oscillators[0].t;
-                FileIO.writeSSV(out, t, 
-                        oscillators[0].y,//振動子１
-                        oscillators[1].y,//振動子２
-                        oscillators[2].y//振動子３
-                        );
+                FileIO.writeSSV(out,
+                        oscillators[0].t,//時刻
+                        oscillators[0].x,//振動子１
+                        oscillators[1].x,//振動子２
+                        oscillators[2].x//振動子３
+                );
             }
         }
     }
